97cf4671beb4fe080c1d35cc06591a1aeda6dd6b
Amaury Aparicio <amaurysunstar@gmail.com>
2021-10-06 13:50:06 -0500

    CLI modified

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,8 @@
 #! /usr/bin/env node
 
+/**
+ * Create a funtion to manage commands
+ */
 const { execSync } = require("child_process");
 
 const runCommand = command => {
@@ -12,19 +15,61 @@ const runCommand = command => {
   return true;
 };
 
+/**
+ * Get the new repository name
+ */
+
 const repoName = process.argv[2];
 
-const gitCheckoutCommand = `git clone -n --depth 1 https://github.com/AmauryAparicio/express-template ${repoName}`;
+/**
+ * Get the platform
+ */
 
-const installDepsCommand = `cd ${repoName} && npm install`;
+const os = require("os").platform();
+
+const isWindows = /^win/.test(os);
+
+/**
+ * Clone the repository
+ */
+
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/AmauryAparicio/express-template ${repoName}`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 
 const checkedOut = runCommand(gitCheckoutCommand);
+
 if (!checkedOut) process.exit(-1);
+
+/**
+ * Initialize the new repository
+ */
+
+const newRepoCommand = isWindows
+  ? `cd ${repoName} && RMDIR /Q/S .git`
+  : `cd ${repoName} && rm -r -y .git`;
+
+console.log(`Creating new repository for ${repoName}`);
+
+const createdRepo = runCommand(newRepoCommand);
+
+if (!createdRepo) process.exit(-1);
+
+/**
+ * Install dependencies
+ */
+
+const installDepsCommand = `cd ${repoName} && npm install`;
+
 console.log(`Installing dependencies for ${repoName}`);
+
 const installedDeps = runCommand(installDepsCommand);
+
 if (!installedDeps) process.exit(-1);
+
+/**
+ * Exit
+ */
 
 console.log(
   `Congratulations! You are ready. Follow following commands to start.`
