b8ef4ce245bfd3b873d1c0b63d7335de729c4b54
Amaury Aparicio <amaurysunstar@gmail.com>
2021-10-11 18:37:02 -0500

    creates .gitignore

--- src/index.js
+++ src/index.js
@@ -18,7 +18,11 @@ module.exports = async input => {
   console.log(`Installing dependecies using ${input.pkgManager}...`);
   util.editPackageJson(`${targetPath}/package.json`, input.destinationFolder);
   await util.installNodeModules(input.pkgManager, targetPath);
-  fs.appendFile(".gitignore", "node_modules", () => {});
+  fs.appendFile(
+    `${input.destinationFolder}/.gitignore`,
+    "node_modules",
+    () => {}
+  );
   execSync(
     `cd ${targetPath} && git init && git add --all && git commit -m 'First Commit' & cd ..`
   );
