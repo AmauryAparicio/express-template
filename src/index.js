1c035e69d59b5cfbbf782463bc42cc7f6f1e9c0f
Amaury Aparicio <amaurysunstar@gmail.com>
2021-10-11 18:42:24 -0500

    creates .gitignore inside folder then git init

--- src/index.js
+++ src/index.js
@@ -18,10 +18,11 @@ module.exports = async input => {
   console.log(`Installing dependecies using ${input.pkgManager}...`);
   util.editPackageJson(`${targetPath}/package.json`, input.destinationFolder);
   await util.installNodeModules(input.pkgManager, targetPath);
-  fs.appendFile(`${targetPath}/.gitignore`, "node_modules", () => {});
-  execSync(
-    `cd ${targetPath} && git init && git add --all && git commit -m 'First Commit' & cd ..`
-  );
+  fs.appendFile(`${targetPath}/.gitignore`, "node_modules", () => {
+    execSync(
+      `cd ${targetPath} && git init && git add --all && git commit -m 'First Commit' & cd ..`
+    );
+  });
 
   return;
 };
